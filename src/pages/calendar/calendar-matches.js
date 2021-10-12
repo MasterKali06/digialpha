62d3e63a481a3ddee9f7a52c63811d6ed25b6099
mohammad <mohammadmasterr@gmail.com>
2021-10-12 13:51:41 +0330

    new

--- src/pages/calendar/calendar-matches.js
+++ src/pages/calendar/calendar-matches.js
@@ -2,8 +2,9 @@ import "../../scss/pages/calendar/calendar-matches.scss"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useState, connect } from "react"
 import { getMatches } from "../../redux/actions/getMatches";
-import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
+import { Calendar } from 'react-modern-calendar-datepicker';
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { colors } from "../../constants/constants";
 
 const CalendarMatches = () => {
 
@@ -11,6 +12,11 @@ const CalendarMatches = () => {
     const gameId = useSelector(state => state.gameId)
 
     let date = new Date()
+    const today = {
+        year: date.getFullYear(),
+        month: date.getMonth(),
+        day: date.getDay()
+    }
 
     let epoch = date.getTime()
     let offset = (date.getHours() * 3600) + (date.getMinutes() * 60) + date.getSeconds()
@@ -29,7 +35,7 @@ const CalendarMatches = () => {
     }
 
     return (
-        <CalendarMatchesUi onTimeChange={(newEpoch) => onTimeChange(newEpoch)} />
+        <CalendarMatchesUi onTimeChange={(newEpoch) => onTimeChange(newEpoch)} epoch={today} />
     )
 }
 
@@ -38,10 +44,19 @@ const CalendarMatchesUi = (props) => {
 
     const past = useSelector(state => state.pastMatches)
 
+    const [selectedDay, setSelectedDay] = useState(props.today)
+
     return (
         <div className="matches-container">
+            <div className="date-picker-container">
+                <Calendar
+                    colorPrimary={colors.auraGreen}
+                    value={selectedDay}
+                    onChange={setSelectedDay}
+                    shouldHighlightWeekends
+                />
+            </div>
 
-            <Calendar className="calendar" />
 
         </div>
     )
